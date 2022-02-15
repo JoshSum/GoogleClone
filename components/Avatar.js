2149f79dfc0615018c0c4e2fc33c9c190b8cac4e
JoshSum <joshuakuadrat@gmail.com>
2022-02-16 01:12:37 +0700

    🐛 Fix a bug

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,13 @@
+import Image from "next/image"
 
 function Avatar({url, className}) {
-  return <img 
+  return <Image 
       loading="lazy"
       src={url}
       alt="profile pict"
-      className={`rounded-full h-10 w-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+      className={`rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+      width={40}
+      height={40}
       />
       
 }
